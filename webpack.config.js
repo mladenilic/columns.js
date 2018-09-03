8e7a53aea431544a2f980b8cc02e0b63a3f407ff
Mladen Ilic <mladen@stuntcoders.com>
2018-09-03 17:24:31 +0200

    Add scss webpack loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,22 @@
 const path = require('path');
 
 module.exports = {
-    entry: require.resolve('./src/columns.js'),
+    entry: require.resolve('./example/js/main.js'),
+    context: __dirname,
     mode: 'production',
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'columns.min.js'
+        path: path.resolve(__dirname, 'example/dist'),
+        filename: 'bundle.js'
     },
     module: {
         rules: [
             {
                 test: /\.js$/,
                 loader: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
     }
