9f9f4a8b2f31ab00d76909317501d8052937069d
Mladen Ilic <mladen@stuntcoders.com>
2018-09-04 20:57:41 +0200

    Set breakpoints on toggle change

--- site/js/main.js
+++ site/js/main.js
@@ -1,15 +1,44 @@
 import scss from '../scss/styles.scss';
 import Columns from '../../src/columns.js';
 
-let rand = (min, max) => {
+const rand = (min, max) => {
   return min + Math.floor(Math.random() * Math.floor(max));
-}
+};
+
+const debounce = (callback, wait) => {
+    let timeout;
+
+    return () => {
+        var context = this, args = arguments;
+
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+            timeout = null;
+            callback.apply(context, args);
+        }, wait);
+    };
+};
 
 document.addEventListener('DOMContentLoaded', () => {
+    let breakpoints = {
+        480: 2,
+        840: 3,
+        1024: 4,
+    };
     let wrapper = document.getElementById('columns-wrapper');
     let grid = new Columns(wrapper, {
         columns: 3
     });
+
+    document.getElementById('responsive-toggle').addEventListener('change', (e) => {
+        grid.setColumns(e.target.checked ? 1 : 3);
+        grid.setBreakpoints(e.target.checked ? breakpoints : false);
+        grid.update();
+    });
+
+    window.addEventListener('resize', debounce(() => {
+        grid.update();
+    }, 50));
 
     document.getElementById('append').addEventListener('click', () => {
         for (let i = 0; i < 9; i++) {
