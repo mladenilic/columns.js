d48ca0b548a7b10480af407d4e4a108aa0a2b7be
Mladen Ilic <mladenilic480@gmail.com>
2019-02-17 19:54:14 +0100

    Update webpack config to export columns as lib

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,13 @@
 const path = require('path');
 
-module.exports = {
-    entry: {
-        'dist/columns.min': require.resolve('./src/columns.js'),
-        'site/dist/bundle': require.resolve('./site/js/main.js'),
-    },
+module.exports = [{
+    entry: require.resolve('./src/columns.js'),
     context: __dirname,
     mode: 'production',
     output: {
         path: path.resolve(__dirname, './'),
-        filename: '[name].js'
+        filename: 'dist/columns.min.js',
+        library: 'Columns'
     },
     module: {
         rules: [
@@ -23,4 +21,25 @@ module.exports = {
             }
         ]
     }
-};
+},
+{
+    entry: require.resolve('./site/js/main.js'),
+    context: __dirname,
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, './site/dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+            }
+        ]
+    }
+}];
