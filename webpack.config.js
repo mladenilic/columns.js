88317a1e654999ff86602932eeb3e220c5bc3788
Mladen Ilic <mladenilic480@gmail.com>
2019-02-17 19:36:40 +0100

    Add build npm task

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 const path = require('path');
 
 module.exports = {
-    entry: require.resolve('./site/js/main.js'),
+    entry: {
+        'dist/columns.min': require.resolve('./src/columns.js'),
+        'site/dist/bundle': require.resolve('./site/js/main.js'),
+    },
     context: __dirname,
     mode: 'production',
     output: {
-        path: path.resolve(__dirname, 'site/dist'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, './'),
+        filename: '[name].js'
     },
     module: {
         rules: [
