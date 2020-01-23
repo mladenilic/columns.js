711d9644bcc9e1299c76dc02184a99cac11eea33
Mladen Ilic <mladenilic480@gmail.com>
2020-01-23 23:06:49 +0100

    Remove demo site from repo

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-module.exports = [{
+module.exports = {
     entry: require.resolve('./src/columns.js'),
     context: __dirname,
     mode: 'production',
@@ -33,35 +33,4 @@ module.exports = [{
             }
         ]
     }
-},
-{
-    entry: require.resolve('./site/js/main.js'),
-    context: __dirname,
-    mode: 'development',
-    output: {
-        path: path.resolve(__dirname, './site/dist'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    { loader: 'style-loader' },
-                    { loader: 'css-loader' },
-                    { loader: 'postcss-loader' },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            implementation: require('dart-sass')
-                        }
-                    }
-                ]
-            }
-        ]
-    }
-}];
+};
