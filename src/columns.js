fcacc62bf315009e352d46d8d22638375bb2c1c0
Mladen Ilic <mladenilic480@gmail.com>
2020-01-27 14:02:51 +0100

    Use 2 space indentation

--- src/columns.js
+++ src/columns.js
@@ -1,61 +1,61 @@
 export default class Columns {
     constructor(container, options = {}) {
-        this.container = container;
-        this.options = options;
+      this.container = container;
+      this.options = options;
 
-        this.options.breakpoints = this.options.breakpoints || {};
-        this.options.column_class = this.options.column_class || 'column-js';
+      this.options.breakpoints = this.options.breakpoints || {};
+      this.options.column_class = this.options.column_class || 'column-js';
 
-        this.items = Array.from(container.children) || [];
+      this.items = Array.from(container.children) || [];
 
-        this.update();
+      this.update();
     }
 
     columnCount() {
-        let columnCount = this.options.columns;
-        let windowWidth = window.innerWidth;
+      let columnCount = this.options.columns;
+      let windowWidth = window.innerWidth;
 
-        Object.keys(this.options.breakpoints).some((breakpoint) => {
-            if (windowWidth < breakpoint) {
-                return true;
-            }
+      Object.keys(this.options.breakpoints).some((breakpoint) => {
+        if (windowWidth < breakpoint) {
+          return true;
+        }
 
-            columnCount = this.options.breakpoints[breakpoint];
-        });
+        columnCount = this.options.breakpoints[breakpoint];
+      });
 
-        return columnCount;
+      return columnCount;
     }
 
     append(element) {
-        this.items.push(element);
-        this.container.children[this.count++ % this.columnCount()].append(element);
+      this.items.push(element);
+      this.container.children[this.count++ % this.columnCount()].append(element);
 
-        return this;
+      return this;
     }
 
     update() {
-        this.count = 0;
-        let columnCount = this.columnCount();
+      this.count = 0;
+      let columnCount = this.columnCount();
 
-        this.container.dataset.columns = columnCount;
-        this.container.innerHTML = `<div class="${this.options.column_class}"></div>`.repeat(columnCount);
+      this.container.dataset.columns = columnCount;
+      this.container.innerHTML = `<div class="${this.options.column_class}"></div>`.repeat(columnCount);
 
-        this.items.forEach((element) => {
-            this.container.children[this.count++ % columnCount].append(element);
-        });
+      this.items.forEach((element) => {
+        this.container.children[this.count++ % columnCount].append(element);
+      });
 
-        return this;
+      return this;
     }
 
     setBreakpoints(breakpoints = {}) {
-        this.options.breakpoints = breakpoints || {};
+      this.options.breakpoints = breakpoints || {};
     }
 
     setColumns(columns) {
-        this.options.columns = columns;
+      this.options.columns = columns;
     }
 
     setColumnClass(columnClass) {
-        this.options.column_class = columnClass;
+      this.options.column_class = columnClass;
     }
 }
