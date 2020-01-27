19edd52778a4d21a54924335f26264700be7916d
Mladen Ilic <mladenilic480@gmail.com>
2020-01-27 14:33:56 +0100

    Test calculating column count when breakpoints are available

--- test/columns.test.js
+++ test/columns.test.js
@@ -9,7 +9,35 @@ describe('Columns js', function () {
     });
   });
 
-  it('#columnCount', function () {
-    expect(this.columns.columnCount()).to.be.equal(3);
+  describe('#columnCount', function () {
+    context('when window width is 400px', function () {
+      before(function () {
+        this.columns = new Columns(this.container, {
+          columns: 3,
+          breakpoints: {
+            320: 4,
+            600: 5
+          }
+        });
+      });
+
+      it('returns correct number of columns', function () {
+        window.innerWidth = 319;
+        expect(this.columns.columnCount()).to.be.equal(3);
+
+        window.innerWidth = 320;
+        expect(this.columns.columnCount()).to.be.equal(4);
+
+        window.innerWidth = 599;
+        expect(this.columns.columnCount()).to.be.equal(4);
+
+        window.innerWidth = 600;
+        expect(this.columns.columnCount()).to.be.equal(5);
+      });
+    });
+
+    it('returns correct number of columns', function () {
+      expect(this.columns.columnCount()).to.be.equal(3);
+    });
   });
 });
